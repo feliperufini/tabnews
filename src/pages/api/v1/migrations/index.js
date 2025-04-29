08dd58cf6efe8af8a0ae5f4566c27202d0f06dc1
Felipe Andrade <37109398+feliperufini@users.noreply.github.com>
2025-04-29 14:02:05 -0400

    fix: fix migrations endpoint bug

--- src/pages/api/v1/migrations/index.js
+++ src/pages/api/v1/migrations/index.js
@@ -3,39 +3,47 @@ import database from '@infra/database.js'
 import migrationRunner from 'node-pg-migrate'
 
 export default async function migrations(request, response) {
-	const dbClient = await database.getNewClient()
-
-	const defaultMigrationOptions = {
-		dbClient: dbClient,
-		dryRun: true,
-		dir: resolve('infra', 'migrations'),
-		direction: 'up',
-		verbose: true,
-		migrationsTable: 'pgmigrations',
+	const allowedMethods = ['GET', 'POST']
+	if (!allowedMethods.includes(request.method)) {
+		return response.status(405).end()
 	}
 
-	if (request.method === 'GET') {
-		const pendingMigrations = await migrationRunner(defaultMigrationOptions)
+	let dbClient
 
-		await dbClient.end()
+	try {
+		dbClient = await database.getNewClient()
 
-		return response.status(200).json(pendingMigrations)
-	}
-
-	if (request.method === 'POST') {
-		const migratedMigrations = await migrationRunner({
-			...defaultMigrationOptions,
-			dryRun: false,
-		})
-
-		await dbClient.end()
-
-		if (migratedMigrations.length > 0) {
-			return response.status(201).json(migratedMigrations)
+		const defaultMigrationOptions = {
+			dbClient: dbClient,
+			dryRun: true,
+			dir: resolve('infra', 'migrations'),
+			direction: 'up',
+			verbose: true,
+			migrationsTable: 'pgmigrations',
 		}
 
-		return response.status(200).json(migratedMigrations)
-	}
+		if (request.method === 'GET') {
+			const pendingMigrations = await migrationRunner(defaultMigrationOptions)
 
-	return response.status(405).end()
+			return response.status(200).json(pendingMigrations)
+		}
+
+		if (request.method === 'POST') {
+			const migratedMigrations = await migrationRunner({
+				...defaultMigrationOptions,
+				dryRun: false,
+			})
+
+			if (migratedMigrations.length > 0) {
+				return response.status(201).json(migratedMigrations)
+			}
+
+			return response.status(200).json(migratedMigrations)
+		}
+	} catch (error) {
+		console.error(error)
+		throw error
+	} finally {
+		await dbClient.end()
+	}
 }

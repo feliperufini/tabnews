cd6e9f7c079e5ab8e1c79ea992a80da56d9ee75a
Felipe Andrade <37109398+feliperufini@users.noreply.github.com>
2025-04-29 09:53:37 -0400

    fix: api migrations error 500 path

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -5,8 +5,6 @@ test("GET to api/v1/status should return 200", async () => {
   const responseBody = await response.json()
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()
 
-  console.log(responseBody)
-
   expect(responseBody.updated_at).toBe(parsedUpdatedAt)
   expect(responseBody.dependencies.database.postgres_version).toBeGreaterThanOrEqual(17.3)
   expect(responseBody.dependencies.database.max_connections).toBe(100)
